f0cdaea99d87b613dd2c56d45183f647acd66016
Max K <TheZnat7@yandex.ru>
2024-02-22 22:52:14 +0300

    Blog fix

---
+++ src/components/BlogCard/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import style from "./BlogCard.module.css";
+import Star from "../../assets/Star.png";
+
+type Props = {
+  title: string;
+  text: string;
+  rating: number;
+  teg: Array<string>;
+};
+
+const BlogCard = ({ title, text, rating, teg }: Props) => {
+  return (
+    <div className={style.card}>
+      <div className={style.card__info}>
+        <p className={style.card__info__title}>{title}</p>
+        <div className={style.card__info__containerTeg}>
+          {teg.map((dataTeg, index, array) => (
+            <p key={index} className={style.card__info__containerTeg_teg}>
+              # {index + 1 !== array.length ? `${dataTeg}, ` : `${dataTeg}`}
+            </p>
+          ))}
+        </div>
+        <div className={style.card__info__rangeArea}>
+          <p className={style.card__info__range}>{rating}</p>
+          <img
+            alt="рейтинг"
+            src={Star}
+            className={style.card__info__rangeImg}
+          />
+        </div>
+      </div>
+      <div className={style.card__container}>
+        <p className={style.card__container__paragraph}>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
--- src/page/Blog/index.tsx
+++ src/page/Blog/index.tsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Footer from "src/components/Footer";
 import Header from "src/components/Header";
-import style from "./Blog.module.css"
+import style from "./Blog.module.css";
+import BlogCard from "src/components/BlogCard";
 
 const Blog = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://dummyjson.com/posts?limit=12&skip=2&select=title,reactions,userId,body,tags"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setData(data.posts);
+      });
+  }, []);
+
   return (
     <div className="page">
       <Header />
       <main className="main">
-        <h2>Blog</h2>
-        <p>
-          Latest <span>Articles</span>
-        </p>
-      
+        <div className={style.area}>
+          <h2 className={style.area__title}>
+            Latest <span className="highlight">Articles</span>
+          </h2>
+          <div className={style.cardsArea}>
+            {data.map((obj, index) => (
+              <BlogCard
+                title={obj["title"]}
+                text={obj["body"]}
+                rating={obj["reactions"]}
+                teg={obj["tags"]}
+                key={index}
+              />
+            ))}
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
